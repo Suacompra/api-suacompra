1e5222b8727726fa524b305d0684a31be4b62e7f
Douglas <douglas123costa@gmail.com>
2020-04-03 22:48:00 -0300

    busca produto

--- src/main/java/com/suacompra/api/controller/ProdutoController.java
+++ src/main/java/com/suacompra/api/controller/ProdutoController.java
@@ -1,13 +1,19 @@
 package com.suacompra.api.controller;
 
+import com.suacompra.api.model.Produto;
+import com.suacompra.api.model.Supermercado;
 import com.suacompra.api.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "produto")
@@ -16,10 +22,33 @@ public class ProdutoController {
     @Autowired
     ProdutoRepository produtoRepository;
 
+    @Value("${cdn.images}")
+    private String cdnImages;
+
     @GetMapping("/get-one/{id}")
     ResponseEntity getOneProduto(@PathVariable Integer id){
         try {
             return new ResponseEntity<>(produtoRepository.getOne(id), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/buscar-produtos-ofertas/{id}")
+    ResponseEntity buscarProduto(@PathVariable Integer id){
+        try {
+            List<Produto> produtoList = produtoRepository.buscarProdutosSupermercado(id);
+            List<Produto> produtos = new ArrayList<>();
+            if(produtoList!=null && produtoList.size()>0){
+                for(Produto produto: produtoList){
+                    produto.setImagemProduto1(cdnImages+produto.getImagemProduto1());
+                    produto.setImagemProduto2(cdnImages+produto.getImagemProduto2());
+                    produto.setImagemProduto3(cdnImages+produto.getImagemProduto3());
+                    produtos.add(produto);
+                }
+            }
+            return new ResponseEntity<>(produtos,HttpStatus.OK);
         }catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
--- src/main/java/com/suacompra/api/repository/ProdutoRepository.java
+++ src/main/java/com/suacompra/api/repository/ProdutoRepository.java
@@ -2,6 +2,12 @@ package com.suacompra.api.repository;
 
 import com.suacompra.api.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProdutoRepository extends JpaRepository<Produto,Integer> {
+
+    @Query(value = "SELECT * FROM produto WHERE ativo = true AND id_supermercado = ?1",nativeQuery = true)
+    List<Produto> buscarProdutosSupermercado(Integer id);
 }
